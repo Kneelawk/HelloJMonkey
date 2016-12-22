e00bcfb4c48a8c7058f768222f3e352fe7eea4fd
Kneelawk <jedidiah.pommert@gmail.com>
2016-12-22 02:24:07 -0800

    Initial Upload Commit

---
+++ src/launcher/java/org/kneelawk/hellojmonkeylauncher/HelloJMonkeyLauncher.java
@@ -0,0 +1,19 @@
+package org.kneelawk.hellojmonkeylauncher;
+
+import java.io.IOException;
+
+public class HelloJMonkeyLauncher {
+	public static void main(String[] args) {
+		CPControl3 cp = new CPControl3("org.kneelawk.hellojmonkey.HelloJMonkey");
+
+		cp.addExtractingFromFileLibrary(CPControl3.ME)
+				.addLibrary("application", new CPControl3.DirectoryEntryFilter("app"), CPControl3.ALWAYS_DELETE)
+				.addLibrary("libraries", new CPControl3.DirectoryEntryFilter("libs"), CPControl3.ALWAYS_DELETE);
+
+		try {
+			cp.launch(args);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
