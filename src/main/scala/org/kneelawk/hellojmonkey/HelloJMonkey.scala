0c5dd01406573fa485b91a1679fcc6eeb89dd4d6
Kneelawk <jedidiah.pommert@gmail.com>
2018-04-08 02:30:05 -0700

    Add logger back end

--- src/main/scala/org/kneelawk/hellojmonkey/HelloJMonkey.scala
+++ src/main/scala/org/kneelawk/hellojmonkey/HelloJMonkey.scala
@@ -8,16 +8,19 @@ import com.jme3.scene.shape.Box
 import com.jme3.scene.Geometry
 import com.jme3.material.Material
 import com.jme3.math.ColorRGBA
+import org.log4s._
 
 object HelloJMonkey extends SimpleApplication {
+  val log = getLogger
+
   def main(args: Array[String]) {
-    println("Starting up...")
+    log.info("Starting up...")
     setShowSettings(false)
     start
   }
 
   def simpleInitApp() {
-    println("Init simple app")
+    log.info("Init simple app")
     val box = new Box(1, 1, 1)
     val geom = new Geometry("Box", box)
     val mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
