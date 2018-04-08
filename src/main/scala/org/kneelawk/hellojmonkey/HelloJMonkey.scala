37446b0f61fceea772c70150c94935b099680f57
Kneelawk <jedidiah.pommert@gmail.com>
2018-04-08 13:57:10 -0700

    Add filters

--- src/main/scala/org/kneelawk/hellojmonkey/HelloJMonkey.scala
+++ src/main/scala/org/kneelawk/hellojmonkey/HelloJMonkey.scala
@@ -9,6 +9,9 @@ import com.jme3.scene.Geometry
 import com.jme3.material.Material
 import com.jme3.math.ColorRGBA
 import org.log4s._
+import com.jme3.post.FilterPostProcessor
+import com.jme3.post.filters.BloomFilter
+import com.jme3.post.filters.LightScatteringFilter
 
 object HelloJMonkey extends SimpleApplication {
   val log = getLogger
@@ -21,11 +24,20 @@ object HelloJMonkey extends SimpleApplication {
 
   def simpleInitApp() {
     log.info("Init simple app")
+
+    log.trace("Add box")
     val box = new Box(1, 1, 1)
     val geom = new Geometry("Box", box)
     val mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md")
     mat.setColor("Color", ColorRGBA.Blue)
     geom.setMaterial(mat)
     rootNode.attachChild(geom)
+
+    log.trace("Add bloom")
+    val fpp = new FilterPostProcessor(assetManager)
+    val bloom = new BloomFilter
+    bloom.setBlurScale(1.2f)
+    fpp.addFilter(bloom)
+    viewPort.addProcessor(fpp)
   }
 }
